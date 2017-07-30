6aa0dcc0120319d1b02642486c4f2a2d11991e7e
sullof <sullof@gmail.com>
2017-07-30 14:07:29 -0700

    Redirecting aliases

--- start-docker.sh
+++ start-docker.sh
@@ -3,5 +3,5 @@
 docker run \
   --name 0xnil \
   -p 9092 \
-  -e VIRTUAL_HOST=0xnil.com,www.0xnil.com,0xnil.org,www.0xnil.org \
+  -e VIRTUAL_HOST=0xnil.com,www.0xnil.com,0xnil.org,www.0xnil.org,oxnil.org,www.oxnil.org \
   -d 0xnil
