5d6bae04c89e46dc101a4a04e43b74d4d9e64eeb
sullof <sullof@gmail.com>
2017-12-28 23:54:25 -0800

    http 8

--- bin/docker-node.sh
+++ bin/docker-node.sh
@@ -11,7 +11,7 @@ source bin/.default.env && docker run -d \
   -v $PWD:/usr/src/app \
   -v /vol/log/0xNIL:/var/log/0xNIL \
   -e VIRTUAL_HOST=oxnil.org,www.oxnil.org,0xnil.com,www.0xnil.com,0xnil.org,www.0xnil.org,oxnil.com,www.oxnil.com \
+  -e LETSENCRYPT_HOST=0xnil.org,www.0xnil.org \
+  -e LETSENCRYPT_EMAIL=admin@0xnil.org \
   -w /usr/src/app node:6 npm run start
 
-#  -e LETSENCRYPT_HOST=0xnil.org,www.0xnil.org \
-#  -e LETSENCRYPT_EMAIL=admin@0xnil.org \
