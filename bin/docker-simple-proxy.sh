9a6c20146d95f1d3bc30a81f40328efed4f719d4
sullof <sullof@gmail.com>
2017-12-29 00:01:37 -0800

    http 9

---
+++ bin/docker-simple-proxy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > /vol/proxy/templates/nginx.tmpl
+
+docker stop nginx
+docker rm nginx
+
+docker run -d \
+  --name proxy \
+  --restart unless-stopped \
+  -v /var/run/docker.sock:/tmp/docker.sock:ro \
+  jwilder/nginx-proxy
