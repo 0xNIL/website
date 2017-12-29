c79873a60732b1eab8f826ae0a21d35ea9be8f8c
sullof <sullof@gmail.com>
2017-12-29 08:35:04 -0800

    http 12

--- static/js/pre-body.js
+++ static/js/pre-body.js
@@ -5,3 +5,10 @@
 // } else if (location.hostname === '0xnil.org' && location.protocol === 'http:') {
 //     location = location.href.replace(/http:/, 'https:')
 // }
+
+
+var aliases = 'www.0xnil.com,oxnil.org,www.oxnil.org,oxnil.com,www.oxnil.com'.split(',')
+
+if (~aliases.indexOf(location.hostname)) {
+    location = location.href.replace(RegExp(location.hostname), '0xnil.com')
+}
