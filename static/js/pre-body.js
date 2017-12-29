161781f6ea5bc5d9c2da0f82b779272b2de31dac
sullof <sullof@gmail.com>
2017-12-28 23:39:52 -0800

    http 5

--- static/js/pre-body.js
+++ static/js/pre-body.js
@@ -1,17 +1,7 @@
 var aliases = '0xnil.com,www.0xnil.com,www.0xnil.org,oxnil.org,www.oxnil.org,oxnil.com,www.oxnil.com'.split(',')
 
-var relocation
-
 if (~aliases.indexOf(location.hostname)) {
-    relocation = location.href//.replace(/https:/, 'http:')
-        .replace(RegExp(location.hostname), '0xnil.org')
+    location = location.href.replace(/http:/, 'https:').replace(RegExp(location.hostname), '0xnil.org')
+} else if (location.hostname === '0xnil.org' && location.protocol === 'http:') {
+    location = location.href.replace(/http:/, 'https:')
 }
-if (//location.hostname === '0xnil.org' &&
-    location.protocol === 'https:') {
-    relocation = (relocation || location).href.replace(/https:/, 'http:')
-}
-
-if (relocation) {
-    location = relocation
-}
-
