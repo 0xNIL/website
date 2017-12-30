07c0c430d4fa4a1446e8d595549ab90bebfaeade
sullof <sullof@gmail.com>
2017-12-29 17:37:37 -0800

    Removing 0xnil.com

--- docker/node.sh
+++ docker/node.sh
@@ -10,8 +10,8 @@ source bin/.default.env && docker run -d \
   --restart unless-stopped \
   -v $PWD:/usr/src/app \
   -v /vol/log/0xNIL:/var/log/0xNIL \
-  -e VIRTUAL_HOST=oxnil.org,www.oxnil.org,0xnil.com,www.0xnil.com,0xnil.org,www.0xnil.org,oxnil.com,www.oxnil.com \
-  -e LETSENCRYPT_HOST=0xnil.org,www.0xnil.org,api.0xnil.org \
+  -e VIRTUAL_HOST=oxnil.org,www.oxnil.org,0xnil.org,www.0xnil.org,oxnil.com,www.oxnil.com \
+  -e LETSENCRYPT_HOST=0xnil.org,www.0xnil.org \
   -e LETSENCRYPT_EMAIL=info@0xnil.org \
   -w /usr/src/app node:6 npm run start
 
