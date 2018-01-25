c5f3ed14627e0ad5004b8abb2a24a228af09f360
sullof <sullof@gmail.com>
2018-01-24 20:51:39 -0800

    Adding docker script

---
+++ docker/node-it.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+docker stop 0xNIL-web
+docker rm 0xNIL-web
+
+source docker/.default.env && docker run -it \
+  --name 0xNIL-web \
+  --link 0xNIL-redis:redis \
+  -p 9092 \
+  -v $PWD:/usr/src/app \
+  -v /vol/log/0xNIL:/var/log/0xNIL \
+  -e VIRTUAL_HOST=oxnil.org,www.oxnil.org,0xnil.org,www.0xnil.org,oxnil.com,www.oxnil.com \
+  -e LETSENCRYPT_HOST=0xnil.org,www.0xnil.org \
+  -e LETSENCRYPT_EMAIL=info@0xnil.org \
+  -w /usr/src/app node:6 npm run start
+
