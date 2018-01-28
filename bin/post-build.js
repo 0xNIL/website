5d2536d38887a66b5290614007a70f96820fd9f7
sullof <sullof@gmail.com>
2018-01-28 09:25:47 -0800

    Fix builder

--- bin/post-build.js
+++ bin/post-build.js
@@ -7,3 +7,9 @@ let dev = fs.readFileSync(path.resolve(__dirname, '../static/debug-index.html'),
 fs.writeFileSync(path.resolve(__dirname, '../static/index.html'), dev)
 
 
+dev = fs.readFileSync(path.resolve(__dirname, '../static/debug-ifo-first-round.html'), 'utf-8')
+.replace(RegExp('id="prebody" integrity=""'), `id="prebody" integrity="sha256-${process.env.PREBODY}"`)
+
+fs.writeFileSync(path.resolve(__dirname, '../static/ifo-first-round.html'), dev)
+
+
